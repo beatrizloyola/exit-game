ce4edbc5bf3ec6f4a3944d6ce89dffc21a013523
Victor Carraly <vcarraly@gmail.com>
2026-06-18 15:54:47 -0300

    Adiciona alavancas
    Adiciona alavanca com animações prontas e conecta com o script usado pelos botões para poder substitui-los

--- scripts/toggle_interactable.gd
+++ scripts/toggle_interactable.gd
@@ -5,43 +5,21 @@ signal state_changed(symbol: String, value: bool)
 @export var symbol := "P"
 @export var is_on := false
 
-@export var moving_part_path: NodePath
-@export var object_visible_when_on_path: NodePath
-
-@export var off_rotation_degrees := Vector3.ZERO
-@export var on_rotation_degrees := Vector3(0, 0, 45)
-
-var moving_part: Node3D
-var object_visible_when_on: Node
-var _color_mesh: MeshInstance3D
-var _material: StandardMaterial3D
+@onready var anim_player: AnimationPlayer = $AnimationPlayer
+var anim_name := "Po_Bo|Level_Down" 
 
 
 func _ready() -> void:
 	add_to_group("interactable")
 
-	if moving_part_path != NodePath():
-		moving_part = get_node_or_null(moving_part_path)
-
-	if object_visible_when_on_path != NodePath():
-		object_visible_when_on = get_node_or_null(object_visible_when_on_path)
-
-	_color_mesh = _find_mesh()
-	if _color_mesh != null:
-		_material = StandardMaterial3D.new()
-		_material.albedo_color = Color.RED
-		_color_mesh.material_override = _material
-
-	atualizar_visual()
-
-
-func _find_mesh() -> MeshInstance3D:
-	if moving_part is MeshInstance3D:
-		return moving_part
-	for child in get_children():
-		if child is MeshInstance3D:
-			return child
-	return null
+	if anim_player != null and anim_player.has_animation(anim_name):
+		if is_on:
+			anim_player.play(anim_name)
+			anim_player.seek(anim_player.current_animation_length, true) 
+		else:
+			anim_player.play(anim_name)
+			anim_player.seek(0.0, true) 
+			anim_player.stop()
 
 
 func interact(player) -> void:
@@ -51,14 +29,11 @@ func interact(player) -> void:
 
 
 func atualizar_visual() -> void:
-	if moving_part != null:
-		moving_part.rotation_degrees = on_rotation_degrees if is_on else off_rotation_degrees
-
-	if object_visible_when_on != null:
-		object_visible_when_on.visible = is_on
-
-	if _material != null:
-		_material.albedo_color = Color.GREEN if is_on else Color.RED
+	if anim_player != null:
+		if is_on:
+			anim_player.play(anim_name)
+		else:
+			anim_player.play_backwards(anim_name)
 
 
 func get_logic_symbol() -> String:
